53b87945ad6828e4bc56d1420ae3fe7d0eb177f0
Mustafa Burak Aydın <mburak.aydin@xenonsmart.com>
2025-10-14 19:26:58 +0300

    created modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { InfrastructureModule } from './infrastructure/infrastructure.module';
+import { DomainModule } from './domain/domain.module';
+import { ApplicationModule } from './application/application.module';
+import { PresentationModule } from './presentation/presentation.module';
 
 @Module({
-  imports: [],
+  imports: [
+    InfrastructureModule,
+    DomainModule,
+    ApplicationModule,
+    PresentationModule,
+  ],
   controllers: [],
   providers: [],
 })
