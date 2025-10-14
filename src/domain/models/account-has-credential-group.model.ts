856400f5d5a464491ecda2e02884fd1bd0eb9eef
Mustafa Burak Aydın <mburak.aydin@xenonsmart.com>
2025-10-14 20:55:10 +0300

    create models

--- src/domain/models/account-has-credential-group.model.ts
+++ src/domain/models/account-has-credential-group.model.ts
@@ -1,27 +1,17 @@
-// export class AccountHasCredentialGroupModel {
-//   public readonly id?: string;
-//   public readonly email: string | null;
-//   public readonly firstName: string | null;
-//   public readonly lastName: string | null;
-//   public readonly password: string | null;
-//   public readonly accessToken: string | null;
-//   public readonly refreshToken: string | null;
-//   public readonly verifiedAt: Date | null;
-//   public readonly createdAt: Date | null;
-//   public readonly updatedAt: Date | null;
-//   public readonly deletedAt: Date | null;
+export class AccountHasCredentialGroupModel {
+  public readonly id?: string;
+  public readonly accountId: string | null;
+  public readonly credentialGroupId: string | null;
+  public readonly createdAt: Date | null;
+  public readonly updatedAt: Date | null;
+  public readonly deletedAt: Date | null;
 
-//   constructor(props: Partial<AccountHasCredentialGroupModel> = {}) {
-//     this.id = props.id;
-//     this.email = props.email ?? null;
-//     this.firstName = props.firstName ?? null;
-//     this.lastName = props.lastName ?? null;
-//     this.password = props.password ?? null;
-//     this.accessToken = props.accessToken ?? null;
-//     this.refreshToken = props.refreshToken ?? null;
-//     this.verifiedAt = props.verifiedAt ?? null;
-//     this.createdAt = props.createdAt ?? null;
-//     this.updatedAt = props.updatedAt ?? null;
-//     this.deletedAt = props.deletedAt ?? null;
-//   }
-// }
+  constructor(props: Partial<AccountHasCredentialGroupModel> = {}) {
+    this.id = props.id;
+    this.accountId = props.accountId ?? null;
+    this.credentialGroupId = props.credentialGroupId ?? null;
+    this.createdAt = props.createdAt ?? null;
+    this.updatedAt = props.updatedAt ?? null;
+    this.deletedAt = props.deletedAt ?? null;
+  }
+}
--- src/domain/repositories/account.repository.interface.ts
+++ src/domain/repositories/account.repository.interface.ts
@@ -1,6 +1,6 @@
-import { Accounts } from 'src/infrastructure/database/postgres/entities/Accounts';
-import { IBaseRepository } from './account-has-credential-groups.repository.interface';
-export interface IAccountRepository
-  extends IBaseRepository<Accounts, AccountModel> {}
+// import { Accounts } from 'src/infrastructure/database/postgres/entities/Accounts';
+// import { IBaseRepository } from './account-has-credential-groups.repository.interface';
+// export interface IAccountRepository
+//   extends IBaseRepository<Accounts, AccountModel> {}
 
-export const IAccountRepository = Symbol('IAccountRepository');
+// export const IAccountRepository = Symbol('IAccountRepository');
