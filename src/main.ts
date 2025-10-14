3ac1f38eb801fe62e83a5a38480d404111de08ea
Mustafa Burak Aydın <mburak.aydin@xenonsmart.com>
2025-10-14 19:44:53 +0300

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EnvironmentConfigService } from './application/environment-config/environment-config.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const environmentConfigService = app.get(EnvironmentConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('Pass In Closet')
+    .setDescription('Keep Your Pass In Closet')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(
     environmentConfigService.appEnvironmentConfigService.getAppPort() ?? 3000,
